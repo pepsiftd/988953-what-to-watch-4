5e429f7bb6bdd6b87bc6079ec031f6635be06314
pepsiftd <pepsiftd@gmail.com>
2020-07-09 19:59:19 +0300

    Добавляет компонент VideoPlayer

--- src/components/small-movie-card/small-movie-card.jsx
+++ src/components/small-movie-card/small-movie-card.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SmallMovieCard = ({movie, handleHover}) => {
-  const {title, imageSrc, movieLink} = movie;
+import {VideoPlayer} from '@/components/video-player/video-player';
+
+const SmallMovieCard = ({movie, handleHover, isPreviewPlaying}) => {
+  const {title, imageSrc, movieLink, preview} = movie;
 
   return (
     <article className="small-movie-card catalog__movies-card"
@@ -11,7 +13,14 @@ const SmallMovieCard = ({movie, handleHover}) => {
       }}
     >
       <div className="small-movie-card__image">
-        <img src={imageSrc} alt={title} width="280" height="175" />
+        <VideoPlayer
+          src={preview}
+          poster={imageSrc}
+          isPlaying={isPreviewPlaying}
+          isMute={true}
+          width="280"
+          height="175"
+        />
       </div>
       <h3 className="small-movie-card__title">
         <a className="small-movie-card__link" href={movieLink}>{title}</a>
@@ -25,8 +34,10 @@ SmallMovieCard.propTypes = {
     title: PropTypes.string.isRequired,
     imageSrc: PropTypes.string.isRequired,
     movieLink: PropTypes.string.isRequired,
+    preview: PropTypes.string.isRequired,
   }).isRequired,
-  handleHover: PropTypes.func.isRequired
+  handleHover: PropTypes.func.isRequired,
+  isPreviewPlaying: PropTypes.bool.isRequired,
 };
 
 export {SmallMovieCard};
---
+++ src/components/video-player/video-player.jsx
@@ -0,0 +1,69 @@
+import React, {PureComponent, createRef} from 'react';
+import PropTypes from 'prop-types';
+
+class VideoPlayer extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this._videoRef = createRef();
+
+    this.state = {
+      isMute: this.props.isMute,
+      isPlaying: this.props.isPlaying,
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    const {src, poster, width, height} = this.props;
+    const {isMute} = this.state;
+    const video = this._videoRef.current;
+
+    video.src = src;
+    video.poster = poster;
+    video.muted = isMute;
+    video.width = width;
+    video.height = height;
+
+    video.oncanplaythrough = () => {
+      this.setState({
+        isLoading: false,
+      });
+    };
+  }
+
+  componentWillUnmount() {
+    const video = this._videoRef.current;
+
+    video.src = ``;
+    video.poster = ``;
+    video.oncanplaythrough = null;
+  }
+
+  render() {
+    return (
+      <video
+        ref={this._videoRef}
+      />
+    );
+  }
+
+  componentDidUpdate() {
+    const video = this._videoRef.current;
+
+    if (this.props.isPlaying) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  }
+}
+
+VideoPlayer.propTypes = {
+  src: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  isPlaying: PropTypes.bool.isRequired,
+  isMute: PropTypes.bool.isRequired,
+};
+
+export {VideoPlayer};
